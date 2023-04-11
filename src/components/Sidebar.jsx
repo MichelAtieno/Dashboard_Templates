b64da64f1c3a4190bc1ea8e4af9b3bd2b0ead22b
MichelAtieno <michelatieno23@gmail.com>
2023-04-11 10:27:39 +0300

    Added sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -11,6 +11,10 @@ import { FiLogOut } from "react-icons/fi";
 import { VscChromeClose } from "react-icons/vsc";
 
 export default function Sidebar() {
+
+  const [currentLink, setCurrentLink] = useState(1)
+
+
   return (
     < >
     <Section>
@@ -22,37 +26,49 @@ export default function Sidebar() {
         <div className="toggle"></div>
         <div className="links">
           <ul>
-            <li>
+            <li 
+              onClick={() => setCurrentLink(1)}
+              className={currentLink === 1 ? "active" : ""}>
               <a href="#">
               <MdSpaceDashboard />
               <span>Dashboard</span>
               </a>
             </li>
-            <li>
+            <li 
+              onClick={() => setCurrentLink(2)}
+              className={currentLink === 2 ? "active" : ""}>
               <a href="#">
               <RiDashboard2Fill />
               <span>Riders</span>
               </a>
             </li>
-            <li>
+            <li
+              onClick={() => setCurrentLink(3)}
+              className={currentLink === 3 ? "active" : ""}>
               <a href="#">
               <FaAddressCard />
               <span>Payment Details</span>
               </a>
             </li>
-            <li>
+            <li 
+              onClick={() => setCurrentLink(4)}
+              className={currentLink === 4 ? "active" : ""}>
               <a href="#">
               <GiTwirlCenter />
               <span>Learning Center</span>
               </a>
             </li>
-            <li>
+            <li
+              onClick={() => setCurrentLink(5)}
+              className={currentLink === 5 ? "active" : ""}>
               <a href="#">
               <BsFillChatTextFill />
               <span>FAQs</span>
               </a>
             </li>
-            <li>
+            <li 
+              onClick={() => setCurrentLink(6)}
+              className={currentLink === 6 ? "active" : ""}>
               <a href="#">
               <IoSettings />
               <span>Settings</span>
@@ -81,5 +97,107 @@ const Section = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-between;
+  padding: 2rem 0;
+  gap: 2rem;
+  .top {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    width: 100%;
+    .toggle {
+      display: none;
+    }
+    .brand {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 2rem;
+      svg {
+        color: #ffc107;
+        font-size: 2rem;
+      }
+      span {
+        font-size: 2rem;
+        color: #ffc107;
+        font-family: "Permanent Marker", cursive;
+      }
+    }
+    .links {
+      display: flex;
+      justify-content: center;
+      ul {
+        list-style-type: none;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        li {
+          padding: 0.6rem 1rem;
+          border-radius: 0.6rem;
+          &:hover {
+            background-color: #ffc107;
+            a {
+              color: black;
+            }
+          }
+          a {
+            text-decoration: none;
+            display: flex;
+            gap: 1rem;
+            color: white;
+          }
+        }
+        .active {
+          background-color: #ffc107;
+          a {
+            color: black;
+          }
+        }
+      }
+    }
+  }
+  .logout {
+    padding: 0.3rem 1rem;
+    border-radius: 0.6rem;
+    &:hover {
+      background-color: #da0037;
+    }
+    a {
+      text-decoration: none;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      color: white;
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    position: initial;
+    width: 100%;
+    height: max-content;
+    padding: 1rem;
+    .top {
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0 1rem;
+      .toggle {
+        display: block;
+        color: white;
+        z-index: 99;
+        svg {
+          font-size: 1.4rem;
+        }
+      }
+      .brand {
+        gap: 1rem;
+        justify-content: flex-start;
+      }
+    }
+    .top > .links,
+    .logout {
+      display: none;
+    }
+  }
 
 `;
